60bdcbb69e7f6c1d9831850149d9c11d512b8df6
OoShawnoO <840831038@qq.com>
2024-04-14 13:51:54 +0800

    [+] 2024.4.14 跨平台

--- Mole.cpp
+++ Mole.cpp
@@ -13,8 +13,14 @@
 #include <sstream>
 #include "Mole.h"
 
-namespace hzd {
+#ifdef __linux__
 #define CANCEL_COLOR_SCHEME "\033[0m"
+#elif _WIN32
+#include <windows.h>
+#undef ERROR
+#endif
+
+namespace hzd {
 
     bool Mole::is_stop = false;
     std::unordered_map<std::string,std::shared_ptr<Mole::LogChannel>>  Mole::channel_map;
@@ -37,6 +43,7 @@ namespace hzd {
             {"FATAL",Mole::LogItem::LogLevel::FATAL}
     };
 
+#ifdef __linux__
     const std::unordered_map<Mole::LogItem::LogLevel,const char*> Mole::color_scheme{
             {Mole::LogItem::LogLevel::TRACE,"\033[36m"},
             {Mole::LogItem::LogLevel::INFO,"\033[32m"},
@@ -44,6 +51,22 @@ namespace hzd {
             {Mole::LogItem::LogLevel::ERROR,"\033[31m"},
             {Mole::LogItem::LogLevel::FATAL,"\033[31m"}
     };
+#elif _WIN32
+    const std::unordered_map<Mole::LogItem::LogLevel,const char*> Mole::color_scheme{
+            {Mole::LogItem::LogLevel::TRACE,"1"},
+            {Mole::LogItem::LogLevel::INFO,"2"},
+            {Mole::LogItem::LogLevel::WARN,"6"},
+            {Mole::LogItem::LogLevel::ERROR,"12"},
+            {Mole::LogItem::LogLevel::FATAL,"4"}
+    };
+
+    template<typename T>
+    void ColorCout(T t,const int ForeColor=7,const int BackColor=0){
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),ForeColor+BackColor*0x10);
+        std::cout << t;
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),7);
+    }
+#endif
 
     Semaphore::Semaphore(size_t count) : count(count) {
 
@@ -77,11 +100,152 @@ namespace hzd {
         }
     }
 
+    [[nodiscard]] std::string Mole::LogChannel::Variable::Detail() const {
+        std::string data_string;
+        switch(type) {
+            case INT8: data_string = "type:int8_t value:" + std::to_string(data.int8_);
+                break;
+            case INT16: data_string = "type:int16_t value:" + std::to_string(data.int16_);
+                break;
+            case INT32: data_string = "type:int32_t value:" + std::to_string(data.int32_);
+                break;
+            case INT64: data_string = "type:int64_t value:" + std::to_string(data.int64_);
+                break;
+            case UINT8: data_string = "type:uint8_t value:" + std::to_string(data.uint8_);
+                break;
+            case UINT16: data_string = "type:uint16_t value:" + std::to_string(data.uint16_);
+                break;
+            case UINT32: data_string = "type:uint32_t value:" + std::to_string(data.uint32_);
+                break;
+            case UINT64: data_string = "type:uint64_t value:" + std::to_string(data.uint64_);
+                break;
+            case FLOAT: data_string = "type:float value:" + std::to_string(data.float_);
+                break;
+            case DOUBLE: data_string = "type:double value:" + std::to_string(data.double_);
+                break;
+            case LONG_DOUBLE: data_string = "type:long double value:" + std::to_string(data.ldouble_);
+                break;
+            case CONST_CHAR_PTR: data_string = "type:const char* value:" + std::string(data.constcharptr_);
+                break;
+            case CHAR_PTR: data_string = "type:char* value:" + std::string(data.charptr_);
+                break;
+        }
+        return "name:" + std::string(name) + " " + data_string + "\n";
+    }
+
     Mole::LogChannel::LogChannel(
             std::string channel_name
     ):channel_name(std::move(channel_name)){
     }
 
+
+
+#define LOG_ITEM(type) do { \
+        if(static_cast<int>(type) < level) return;              \
+        std::string variable_string;                            \
+        if(!variables.empty()){                                 \
+            variable_string += "----------------------------\n";\
+            for(const auto& v : variables ){                    \
+                variable_string += v.Detail();                  \
+            }                                                   \
+            variable_string += "----------------------------\n";\
+        }                                                       \
+        LogItem item{type,content,std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()),line,file_name,variable_string};\
+        commit_channel.Push(std::move(item));                   \
+}while(0)
+
+
+    // FATAL级日志
+    void Mole::LogChannel::Fatal(
+            const char* content,
+            const char* file_name,
+            unsigned int line,
+            const std::vector<Variable>& variables
+    ) noexcept {
+        std::string _temp = content;
+        Fatal(_temp,file_name,line,variables);
+    }
+    void Mole::LogChannel::Fatal(
+            const std::string& content,
+            const char* file_name,
+            unsigned int line,
+            const std::vector<Variable>& variables
+    ) noexcept {
+        LOG_ITEM(LogItem::FATAL);
+    }
+
+    // ERROR级日志
+    void Mole::LogChannel::Error(
+            const char* content,
+            const char* file_name,
+            unsigned int line,
+            const std::vector<Variable>& variables
+    ) noexcept {
+        std::string _temp = content;
+        Error(_temp,file_name,line,variables);
+    }
+    void Mole::LogChannel::Error(
+            const std::string& content,
+            const char* file_name,
+            unsigned int line,
+            const std::vector<Variable>& variables
+    ) noexcept {
+        LOG_ITEM(LogItem::ERROR);
+    }
+    // WARN级日志
+    void Mole::LogChannel::Warn(
+            const char* content,
+            const char* file_name,
+            unsigned int line,
+            const std::vector<Variable>& variables
+    ) noexcept {
+        std::string _temp = content;
+        Warn(_temp,file_name,line,variables);
+    }
+    void Mole::LogChannel::Warn(
+            const std::string& content,
+            const char* file_name,
+            unsigned int line,
+            const std::vector<Variable>& variables
+    ) noexcept {
+        LOG_ITEM(LogItem::WARN);
+    }
+    // INFO级日志
+    void Mole::LogChannel::Info(
+            const char* content,
+            const char* file_name,
+            unsigned int line,
+            const std::vector<Variable>& variables
+    ) noexcept {
+        std::string _temp = content;
+        Info(_temp,file_name,line,variables);
+    }
+    void Mole::LogChannel::Info(
+            const std::string& content,
+            const char* file_name,
+            unsigned int line,
+            const std::vector<Variable>& variables
+    ) noexcept {
+        LOG_ITEM(LogItem::INFO);
+    }
+    // TRACE级日志
+    void Mole::LogChannel::Trace(
+            const char* content,
+            const char* file_name,
+            unsigned int line,
+            const std::vector<Variable>& variables
+    ) noexcept {
+        std::string _temp = content;
+        Trace(_temp,file_name,line,variables);
+    }
+    void Mole::LogChannel::Trace(
+            const std::string& content,
+            const char* file_name,
+            unsigned int line,
+            const std::vector<Variable>& variables
+    ) noexcept {
+        LOG_ITEM(LogItem::TRACE);
+    }
 
     Mole::LogChannel &Mole::Channel(const std::string &channel_name) {
         static Mole mole;
@@ -100,52 +264,61 @@ namespace hzd {
     void Mole::LogChannel::__WriteLogItem(Mole::LogItem &item) {
         char time_buffer[20] = {0};
         std::strftime(time_buffer,sizeof(time_buffer),"%Y-%m-%d %H:%M:%S",std::localtime(&item.time));
-        bzero(item_buffer,sizeof(item_buffer));
+        memset(item_buffer,0,sizeof(item_buffer));
 
         if(item.content.size() + strlen(item.file_name) + channel_name.size()  > 4000 ) {
             std::cerr << "[WARN] single log data memory bigger than 4kb." << std::endl;
             std::stringstream stream;
-            stream  << "[" << item.time << "]"
-                    << "[ " << channel_name << " ]"
-                    << "[ " << level_map.at(item.level) << " ] "
+            stream  << item.time
+                    << " [ " << level_map.at(item.level) << "] "
                     << item.content
+                    << " =>[" << channel_name << "] "
                     << "[" << item.file_name << ":"
                     << item.line << "]\n"
                     << item.variables;
             if(fp) {
                 fwrite(write_buffer,write_cursor,1,fp);
-                bzero(write_buffer,sizeof(write_buffer));
+                memset(write_buffer,0,sizeof(write_buffer));
                 write_cursor = 0;
                 fwrite(stream.str().c_str(),stream.str().size(),1,fp);
             }
+#ifdef __linux__
             std::cout << color_scheme.at(item.level) << stream.str() << CANCEL_COLOR_SCHEME;
+#elif _WIN32
+            ColorCout(stream.str(),std::strtol(color_scheme.at(item.level),nullptr,10));
+#endif
             fflush(stdout);
             return;
         }
 
         sprintf(
                 item_buffer,
-                "[%s][%s][%s] %s [%s:%d]\n%s",
+                "%s [%s] %s =>[%s] [%s:%d]\n%s",
                 time_buffer,
                 level_map.at(item.level),
-                channel_name.c_str(),
                 item.content.c_str(),
+                channel_name.c_str(),
                 item.file_name,
                 item.line,
                 item.variables.c_str()
         );
-        std::cout << color_scheme.at(item.level) << item_buffer << CANCEL_COLOR_SCHEME;
-        fflush(stdout);
-
         if(fp) {
             strcat(write_buffer,item_buffer);
             write_cursor += strlen(item_buffer);
             if(write_cursor >= 4096 * 9) {
                 fwrite(write_buffer,write_cursor,1,fp);
-                bzero(write_buffer,sizeof(write_cursor));
+                memset(write_buffer,0,sizeof(write_buffer));
                 write_cursor = 0;
             }
         }
+#ifdef __linux__
+        std::cout << color_scheme.at(item.level) << item_buffer << CANCEL_COLOR_SCHEME;
+#elif _WIN32
+        ColorCout(item_buffer,std::strtol(color_scheme.at(item.level),nullptr,10));
+#endif
+        fflush(stdout);
+
+
     }
 
     void Mole::LogChannel::SetSaveable(bool is_save_) {
@@ -160,7 +333,7 @@ namespace hzd {
         LogChannel& chan = *log_channel;
         LogItem item{};
         if(chan.is_save) {
-            chan.fp = fopen((std::string("log/") + chan.channel_name).c_str(),"a+");
+            chan.fp = fopen((std::string("log/") + chan.channel_name + ".log").c_str(),"a+");
             if(!chan.fp) std::cerr << "打开或创建log/" << chan.channel_name << "失败" << std::endl;
         }
         while(!is_stop) {
@@ -177,7 +350,7 @@ namespace hzd {
         if(chan.fp) {
             if(strlen(chan.write_buffer) > 0) {
                     fwrite(chan.write_buffer,strlen(chan.write_buffer),1,chan.fp);
-                    bzero(chan.write_buffer,4096 * 10);
+                    memset(chan.write_buffer,0,sizeof(chan.write_buffer));
                     chan.write_cursor = 0;
                 }
             fclose(chan.fp);
@@ -191,7 +364,7 @@ namespace hzd {
             channel_map_iter.second->commit_channel.Push();
         }
         for(auto& t : thread_group) {
-            t.join();
+            if(t.joinable()) t.join();
         }
         thread_group.clear();
     }
--- Mole.h
+++ Mole.h
@@ -16,7 +16,6 @@
 #include <vector>
 #include <thread>
 
-
 namespace hzd {
     // 信号量
     class Semaphore {
@@ -123,48 +122,77 @@ namespace hzd {
     // 日志类
     class Mole {
     public:
+        // 日志项
         struct LogItem {
+            // 日志等级
             enum LogLevel {
                 TRACE,INFO,WARN,ERROR,FATAL
             };
+            // 日志等级
             LogLevel        level;
+            // 日志内容
             std::string     content;
+            // 日志时间
             time_t          time;
+            // 发布日志行号
             unsigned int    line;
+            // 发布日志文件位置
             const char*     file_name;
+            // 日志打包变量内容
             std::string     variables;
         };
+        // 日志频道
         class LogChannel {
         public:
+            // 频道名称
             std::string         channel_name;
+            // 屏蔽日志等级
             LogItem::LogLevel   level{LogItem::TRACE};
+            // 提交队列
             Channel<LogItem>    commit_channel{true};
+            // 日志文件指针
             FILE*               fp{nullptr};
+            // 日志项缓冲区
             char                item_buffer[4096] = {0};
+            // 日志写缓冲区
             char                write_buffer[40960] = {0};
+            // 日志写游标
             size_t              write_cursor{0};
+            // 日志频道是否保存
             bool                is_save{false};
 
+            // 设置日志频道是否保存
             void SetSaveable(bool is_save);
+            // 设置日志屏蔽等级
             void SetLevel(LogItem::LogLevel level);
+            // 构造函数
+            // channel_name 频道名
             explicit LogChannel(std::string  channel_name);
 
+#ifdef _WIN32
+    using int8_t = char;
+    using int16_t = short;
+    using int32_t = int;
+    using int64_t = long long;
+#endif
+
+            // 变量包
             struct Variable {
                 const char* name{nullptr};
                 union data{
-                    int8_t _int8;
-                    int16_t _int16;
-                    int32_t _int32;
-                    int64_t _int64;
-                    uint8_t _uint8;
-                    uint16_t _uint16;
-                    uint32_t _uint32;
-                    uint64_t _uint64;
-                    double _double;
-                    long double _ldouble;
-                    float _float;
-                    char* _charptr;
-                    const char* _constcharptr;
+                    int8_t int8_;
+                    int16_t int16_;
+                    int32_t int32_;
+                    int64_t int64_;
+                    uint8_t uint8_;
+                    uint16_t uint16_;
+                    uint32_t uint32_;
+                    uint64_t uint64_;
+                    double double_;
+                    long double ldouble_;
+                    float float_;
+                    char* charptr_;
+                    const char* constcharptr_;
                 } data{};
                 enum VariableType {
                     INT8,INT16,INT32,INT64,
@@ -172,172 +200,79 @@ namespace hzd {
                     FLOAT,DOUBLE,LONG_DOUBLE,
                     CONST_CHAR_PTR,CHAR_PTR,
                 } type;
-                Variable(const char* n,int8_t v){ name = n;type = INT8; data._int8 = v;};
-                Variable(const char* n,int16_t v){ name = n;type = INT16; data._int16 = v; };
-                Variable(const char* n,int32_t v){ name = n;type = INT32; data._int32 = v; };
-                Variable(const char* n,int64_t v){ name = n;type = INT64; data._int64 = v; };
-                Variable(const char* n,uint8_t v){ name = n;type = UINT8; data._uint8 = v; };
-                Variable(const char* n,uint16_t v){ name = n;type = UINT16; data._uint16 = v; };
-                Variable(const char* n,uint32_t v){ name = n;type = UINT32; data._uint32 = v; };
-                Variable(const char* n,uint64_t v){ name = n;type = UINT64; data._uint64 = v; };
-                Variable(const char* n,float v){ name = n;type = FLOAT; data._float = v; };
-                Variable(const char* n,double v){ name = n;type = DOUBLE; data._double = v; };
-                Variable(const char* n,long double v){ name = n;type = LONG_DOUBLE; data._ldouble = v; };
-                Variable(const char* n,const char* v){ name = n;type = CONST_CHAR_PTR; data._constcharptr = v; };
-                Variable(const char* n,char* v){ name = n;type = CHAR_PTR; data._charptr = v; };
+                Variable(const char* n,int8_t v){ name = n;type = INT8; data.int8_ = v;};
+                Variable(const char* n,int16_t v){ name = n;type = INT16; data.int16_ = v; };
+                Variable(const char* n,int32_t v){ name = n;type = INT32; data.int32_ = v; };
+                Variable(const char* n,int64_t v){ name = n;type = INT64; data.int64_ = v; };
+                Variable(const char* n,uint8_t v){ name = n;type = UINT8; data.uint8_ = v; };
+                Variable(const char* n,uint16_t v){ name = n;type = UINT16; data.uint16_ = v; };
+                Variable(const char* n,uint32_t v){ name = n;type = UINT32; data.uint32_ = v; };
+                Variable(const char* n,uint64_t v){ name = n;type = UINT64; data.uint64_ = v; };
+                Variable(const char* n,float v){ name = n;type = FLOAT; data.float_ = v; };
+                Variable(const char* n,double v){ name = n;type = DOUBLE; data.double_ = v; };
+                Variable(const char* n,long double v){ name = n;type = LONG_DOUBLE; data.ldouble_ = v; };
+                Variable(const char* n,const char* v){ name = n;type = CONST_CHAR_PTR; data.constcharptr_ = v; };
+                Variable(const char* n,char* v){ name = n;type = CHAR_PTR; data.charptr_ = v; };
 
-                [[nodiscard]] std::string Detail() const {
-                    std::string data_string;
-                    switch(type) {
-                        case INT8: data_string = "type:int8_t value:" + std::to_string(data._int8);
-                            break;
-                        case INT16: data_string = "type:int16_t value:" + std::to_string(data._int16);
-                            break;
-                        case INT32: data_string = "type:int32_t value:" + std::to_string(data._int32);
-                            break;
-                        case INT64: data_string = "type:int64_t value:" + std::to_string(data._int64);
-                            break;
-                        case UINT8: data_string = "type:uint8_t value:" + std::to_string(data._uint8);
-                            break;
-                        case UINT16: data_string = "type:uint16_t value:" + std::to_string(data._uint16);
-                            break;
-                        case UINT32: data_string = "type:uint32_t value:" + std::to_string(data._uint32);
-                            break;
-                        case UINT64: data_string = "type:uint64_t value:" + std::to_string(data._uint64);
-                            break;
-                        case FLOAT: data_string = "type:float value:" + std::to_string(data._float);
-                            break;
-                        case DOUBLE: data_string = "type:double value:" + std::to_string(data._double);
-                            break;
-                        case LONG_DOUBLE: data_string = "type:long double value:" + std::to_string(data._ldouble);
-                            break;
-                        case CONST_CHAR_PTR: data_string = "type:const char* value:" + std::string(data._constcharptr);
-                            break;
-                        case CHAR_PTR: data_string = "type:char* value:" + std::string(data._charptr);
-                            break;
-                    }
-                    return "name:" + std::string(name) + " " + data_string + "\n";
-                }
+                // 描述变量名称、类型与数值
+                [[nodiscard]] std::string Detail() const;
             };
 
-#define LOG_ITEM(type) do { \
-        if(static_cast<int>(type) < level) return;              \
-        std::string varibale_string;                            \
-        if(!variables.empty()){                                 \
-            for(const auto& v : variables ){                    \
-                varibale_string += v.Detail();                  \
-            }                                                   \
-        }                                                       \
-        LogItem item{type,content,std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()),line,file_name,varibale_string};\
-        commit_channel.Push(std::move(item));                   \
-}while(0)
-
-            // FATAL级日志
-            void Fatal(
-                    const char* content,
-                    const char* file_name,
-                    unsigned int line,
-                    const std::vector<Variable>& variables = {}
-            ) noexcept {
-                std::string _temp = content;
-                Fatal(_temp,file_name,line,variables);
-            }
-            void Fatal(
-                    const std::string& content,
-                    const char* file_name,
-                    unsigned int line,
-                    const std::vector<Variable>& variables = {}
-            ) noexcept {
-                LOG_ITEM(LogItem::FATAL);
-            }
-            
-            // ERROR级日志
-            void Error(
-                    const char* content,
-                    const char* file_name,
-                    unsigned int line,
-                    const std::vector<Variable>& variables = {}
-            ) noexcept {
-                std::string _temp = content;
-                Error(_temp,file_name,line,variables);
-            }
-            void Error(
-                    const std::string& content,
-                    const char* file_name,
-                    unsigned int line,
-                    const std::vector<Variable>& variables = {}
-            ) noexcept {
-                LOG_ITEM(LogItem::ERROR);
-            }
-            // WARN级日志
-            void Warn(
-                    const char* content,
-                    const char* file_name,
-                    unsigned int line,
-                    const std::vector<Variable>& variables = {}
-            ) noexcept {
-                std::string _temp = content;
-                Warn(_temp,file_name,line,variables);
-            }
-            void Warn(
-                    const std::string& content,
-                    const char* file_name,
-                    unsigned int line,
-                    const std::vector<Variable>& variables = {}
-            ) noexcept {
-                LOG_ITEM(LogItem::WARN);
-            }
-            // INFO级日志
-            void Info(
-                    const char* content,
-                    const char* file_name,
-                    unsigned int line,
-                    const std::vector<Variable>& variables = {}
-            ) noexcept {
-                std::string _temp = content;
-                Info(_temp,file_name,line,variables);
-            }
-            void Info(
-                    const std::string& content,
-                    const char* file_name,
-                    unsigned int line,
-                    const std::vector<Variable>& variables = {}
-            ) noexcept {
-                LOG_ITEM(LogItem::INFO);
-            }
-            // TRACE级日志
-            void Trace(
-                    const char* content,
-                    const char* file_name,
-                    unsigned int line,
-                    const std::vector<Variable>& variables = {}
-            ) noexcept {
-                std::string _temp = content;
-                Trace(_temp,file_name,line,variables);
-            }
-            void Trace(
-                    const std::string& content,
-                    const char* file_name,
-                    unsigned int line,
-                    const std::vector<Variable>& variables = {}
-            ) noexcept {
-                LOG_ITEM(LogItem::TRACE);
-            }
-
+            // FATAL级别日志
+            // content 日志内容
+            // file_name 发布日志位置文件名
+            // line 发布日志位置行号
+            // variables 变量包
+            void Fatal(const char* content,const char* file_name,unsigned int line,const std::vector<Variable>& variables = {}) noexcept;
+            void Fatal(const std::string& content,const char* file_name,unsigned int line,const std::vector<Variable>& variables = {}) noexcept;
+            // ERROR级别日志
+            // content 日志内容
+            // file_name 发布日志位置文件名
+            // line 发布日志位置行号
+            // variables 变量包
+            void Error(const char* content,const char* file_name,unsigned int line,const std::vector<Variable>& variables = {}) noexcept;
+            void Error(const std::string& content,const char* file_name,unsigned int line,const std::vector<Variable>& variables = {}) noexcept;
+            // WARN级别日志
+            // content 日志内容
+            // file_name 发布日志位置文件名
+            // line 发布日志位置行号
+            // variables 变量包
+            void Warn(const char* content,const char* file_name,unsigned int line,const std::vector<Variable>& variables = {}) noexcept;
+            void Warn(const std::string& content,const char* file_name,unsigned int line,const std::vector<Variable>& variables = {}) noexcept;
+            // INFO级别日志
+            // content 日志内容
+            // file_name 发布日志位置文件名
+            // line 发布日志位置行号
+            // variables 变量包
+            void Info(const char* content,const char* file_name,unsigned int line,const std::vector<Variable>& variables = {}) noexcept;
+            void Info(const std::string& content,const char* file_name,unsigned int line,const std::vector<Variable>& variables = {}) noexcept;
+            // TRACE级别日志
+            // content 日志内容
+            // file_name 发布日志位置文件名
+            // line 发布日志位置行号
+            // variables 变量包
+            void Trace(const char* content,const char* file_name,unsigned int line,const std::vector<Variable>& variables = {}) noexcept;
+            void Trace(const std::string& content,const char* file_name,unsigned int line,const std::vector<Variable>& variables = {}) noexcept;
+            // 写日志项
             void __WriteLogItem(LogItem& item);
         };
         // 获取日志频道
         static LogChannel& Channel(const std::string& channel_name = {});
         ~Mole();
+        // 日志等级字符串哈希表
         const static std::unordered_map<std::string,LogItem::LogLevel>      level_str_map;
     private:
+        // 控制日志线程关闭标记
         static bool                                                         is_stop;
+        // 日志频道哈希表
         static std::unordered_map<std::string,std::shared_ptr<LogChannel>>  channel_map;
+        // 日志线程组
         static std::vector<std::thread>                                     thread_group;
-
+        // 日志等级哈希表
         const static std::unordered_map<LogItem::LogLevel,const char*>      level_map;
+        // 日志等级颜色焊锡表
         const static std::unordered_map<LogItem::LogLevel,const char*>      color_scheme;
-
+        // 日志循环
         static void LogLoop(const std::shared_ptr<LogChannel>& log_channel);
     };
 
@@ -347,8 +282,9 @@ namespace hzd {
 #define MOLE_WARN(chan,content,...)
 #define MOLE_INFO(chan,content,...)
 #define MOLE_TRACE(chan,content,...)
-#define MOLE_PACK(variable)
+#define MOLE_VAR(variable)
 #define MOLE_CHANNEL_LEVEL(chan,level)
+#define MOLE_CHANNEL_SAVEABLE(chan,bool)
 #else
     // FATAL级别日志
     // chan 日志频道名
@@ -391,7 +327,7 @@ namespace hzd {
         hzd::Mole::Channel(#chan).Trace(content,__FILE__,__LINE__,##__VA_ARGS__);\
     }while(0)
     // 隐式构造Variable
-#define MOLE_PACK(variable) {#variable,variable}
+#define MOLE_VAR(variable) {#variable,variable}
     // 设置日志频道最低等级
     // chan 日志频道名
     // level 等级字符串
